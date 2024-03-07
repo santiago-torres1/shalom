5babfa2ce0d728092642eb6febb8a1a729b83440
Santiago Torres <Santiago.Torres1407@gmail.com>
2024-03-07 14:25:55 -0500

    search bar toggle enabled

--- src/Header.js
+++ src/Header.js
@@ -2,60 +2,54 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/style.css';
 import './css/responsive.css';
-import { Navbar, Button} from 'react-bootstrap'
+import { Navbar, Nav} from 'react-bootstrap';
+
 
 function Header() {
-  return ( 
-      <div className='hero_area'>
-          <header className="header_section">
-              <Navbar className="custom_nav-container">
-                  <Link to="/" className="navbar-brand">
-                      <span>
-                          Shalom
-                      </span>
-                  </Link>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span className=""></span>
+  return (
+    <div className='hero_area'>
+      <header className="header_section">
+        <Navbar expand="lg" className="custom_nav-container">
+            <Link to="/" className="navbar-brand">
+              <span>Shalom</span>
+            </Link>
+            <Navbar.Toggle aria-controls="navbarSupportedContent" />
+            <Navbar.Collapse id="navbarSupportedContent">
+              <Nav className="navbar-nav mr-auto">
+                <Nav.Item className="nav-item">
+                  <Link to="/App" className="nav-link">Home <span className="sr-only">(current)</span></Link>
+                </Nav.Item>
+                <Nav.Item className="nav-item">
+                  <Link to="/shop" className="nav-link">Shop</Link>
+                </Nav.Item>
+                <Nav.Item className="nav-item">
+                  <Link to="/why" className="nav-link">Why Us</Link>
+                </Nav.Item>
+                <Nav.Item className="nav-item">
+                  <Link to="/testimonial" className="nav-link">Testimonial</Link>
+                </Nav.Item>
+                <Nav.Item className="nav-item">
+                  <Link to="/contact" className="nav-link">Contact Us</Link>
+                </Nav.Item>
+              </Nav>
+              <div className="user_option">
+                <Link to="/login" className="nav-link">
+                  <i className="fa fa-user" aria-hidden="true"></i>
+                  <span>Login</span>
+                </Link>
+                <Link to="/shopping-cart" className="nav-link">
+                  <i className="fa fa-shopping-bag" aria-hidden="true"></i>
+                </Link>
+                <form className="form-inline ">
+                  <button className="btn nav_search-btn" type="submit">
+                    <i className="fa fa-search" aria-hidden="true"></i>
                   </button>
-
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul className="navbar-nav">
-                          <li className="nav-item active">
-                              <Link to="/App" className="nav-link">Home <span className="sr-only">(current)</span></Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link to="/shop" className="nav-link">Shop</Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link to="/why" className="nav-link">Why Us</Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link to="/testimonial" className="nav-link">Testimonial</Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link to="/contact" className="nav-link">Contact Us</Link>
-                          </li>
-                      </ul>
-                      <div className="user_option">
-                          <Link to="/login">
-                              <i className="fa fa-user" aria-hidden="true"></i>
-                              <span>
-                                  Login
-                              </span>
-                          </Link>
-                          <Link to="/shopping-cart">
-                              <i className="fa fa-shopping-bag" aria-hidden="true"></i>
-                          </Link>
-                          <form className="form-inline ">
-                              <button className="btn nav_search-btn" type="submit">
-                                  <i className="fa fa-search" aria-hidden="true"></i>
-                              </button>
-                          </form>
-                      </div>
-                  </div>
-              </Navbar>
-          </header>
-      </div>
+                </form>
+              </div>
+            </Navbar.Collapse>
+        </Navbar>
+      </header>
+    </div>
   );
 }
 
