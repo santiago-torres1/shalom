31ace13482ff1c028f68424ab9924c8857eb1dec
Santiago Torres <Santiago.Torres1407@gmail.com>
2024-03-08 11:16:01 -0500

    toggler animation added, font awesome added, useState added to handle collapsing the navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faShoppingBag, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser, faShoppingBag, faSearch)
 
 const App = () => {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/style.css';
 import '../assets/css/responsive.css';
 import { Navbar, Nav} from 'react-bootstrap';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Header() {
+  const [open, setOpen] = useState()
+
   return (
     <header className="header_section">
       <Navbar expand="lg" sticky="top" className='custom_nav-container'>
           <Link to="/" className="navbar-brand">
             <span>Shalom</span>
           </Link>
-          <Navbar.Toggle aria-controls="navbarSupportedContent" />
-          <Navbar.Collapse id="navbarSupportedContent">
+          <Navbar.Toggle 
+            aria-controls="navbarSupportedContent"
+            aria-expanded={open}
+            onClick={() => setOpen(!open)} 
+            className='custom-toggler'/>
+          <Navbar.Collapse in={open} id="navbarSupportedContent">
             <Nav className="navbar-nav mr-auto">
               <Nav.Item className="nav-item">
-                <Link to="/App" className="nav-link">Inicio <span className="sr-only">(current)</span></Link>
+                <Link to="/App" className="nav-link">Inicio <span className="sr-only"></span></Link>
               </Nav.Item>
               <Nav.Item className="nav-item">
                 <Link to="/shop" className="nav-link">Productos</Link>
@@ -33,15 +39,15 @@ function Header() {
             </Nav>
             <div className="user_option">
               <Link to="/login" className="nav-link">
-                <i className="fa fa-user" aria-hidden="true"></i>
+                <FontAwesomeIcon icon="user" />
                 <span>Inicia Sesion</span>
               </Link>
               <Link to="/shopping-cart" className="nav-link">
-                <i className="fa fa-shopping-bag" aria-hidden="true"></i>
+              <FontAwesomeIcon icon="shopping-bag" />
               </Link>
-              <form className="form-inline ">
+              <form className="form-inline">
                 <button className="btn nav_search-btn" type="submit">
-                  <i className="fa fa-search" aria-hidden="true"></i>
+                <FontAwesomeIcon icon="search" />
                 </button>
               </form>
             </div>
