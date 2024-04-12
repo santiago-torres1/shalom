8cabb89ea0ba57a571a31262e08883a5392b8dd0
Santiago Torres <Santiago.Torres1407@gmail.com>
2024-04-12 16:36:44 -0400

    added spinner

--- src/components/Products/ProductPage.js
+++ src/components/Products/ProductPage.js
@@ -1,4 +1,4 @@
-import { Container, Button } from "react-bootstrap"
+import { Container, Button, Spinner } from "react-bootstrap"
 import { useState, useEffect } from "react"
 import { useParams } from 'react-router-dom'
 import './Products.css'
